62d6af08f4056c3ac5ebd33ff7f7858a470430f6
Aditya <adityanegi7730@gmail.com>
2020-08-28 23:23:00 +0530

    New project added

---
+++ new.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class new
+{
+  public static void main(String[] args) 
+  {
+  	Scanner k =  new Scanner(System.in);
+  	 System.out.println("Enter the range of the pattern");
+  	 int x = k.nextInt();
+
+  	 for(int i=0;i<x;i++)
+  	 {
+       for(int j=0;j<=i;j++)
+       {
+       	System.out.print(j);
+       }
+       System.ot.println();
+  	 }
+  }
+}
