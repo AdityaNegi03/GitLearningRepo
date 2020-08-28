ed6ea976e126fc4916a35e39f83f213994d94e91
Aditya <adityanegi7730@gmail.com>
2020-08-28 23:18:16 +0530

    Initial Commit

---
+++ Learning1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Learning1
+{
+  public static void main(String [] args)
+  {
+    Scanner K = new Scanner(System.in);
+    System.out.println("Hey! This is a beginner program. ");
+  }
+}
